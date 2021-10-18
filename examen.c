df76a66f54dfaa670e8897a7f7c9758e09884844
dianash28 <dianash0128@gmail.com>
2021-10-18 18:28:41 -0500

    correccion

--- examen.c
+++ examen.c
@@ -1,98 +1,114 @@
 #include <stdio.h>
+#include <string.h>
 
 #define  N 10
 #define  M 2
-void pedirClave(int arr[N]);
-void pedirNombre(char nom[N]);
-void pedirEstaturaYSueldo(float dat[N][M]);
-int empleadoMasAlto(float dat[N],int *pos);
-void SueldoMenor(float dato[N]);
-void imprimir(int arr[N], char nombres[N],float datos[N][M]);
+
+void pedirDatos(int arr[N],char nom[N][20],float dat[N][M]);
+int empleadoMasAlto(float dat[N][M]);
+void SueldoMenor(int arr[N], char nom[N][20],float dat[N][M]);
+void imprimir(int arr[N], char nombres[N][20],float datos[N][M]);
+
+
 int main()
 {
     int arreglo[N];
-    char nombres[N];
+    char nombres[N][20];
     float datos[N][M];
-    int posicion=-1;
+    int posicion;
 
-    pedirClave(arreglo);
-    pedirNombre(nombres);
-    pedirEstaturaYSueldo(datos);
-    posicion=empleadoMasAlto(datos, &posicion);
+    pedirDatos(arreglo,nombres,datos);
+    posicion=empleadoMasAlto(datos);
+    SueldoMenor(arreglo,nombres,datos);
     imprimir( arreglo, nombres, datos);
+
+
     fflush(stdin);
     getchar();
     return 0;
 }
 
-void pedirClave(int arr[N])
-{
-    int i;
-    for (i=0; i<N; i++ )
-    {
-        printf("arreglo[%d]", i);
-        scanf("%d",&arr[i]);
-    }
-}
-
-void pedirNombre(char nom[N])
-{
-    int i;
-    for (i=0; i<N; i++ )
-    {
-        printf("arreglo[%d]", i);
-        scanf("%c",&nom[i]);
-    }
-}
-
-void pedirEstaturaYSueldo(float dat[N][M])
+void SueldoMenor(int arr[N], char nom[N][20],float dat[N][M])
 {
     int i,j;
-    for (i=0; i<N; i++ )
-    {
-        for (j=0; j<N; j++ )
-        {
-            printf("arreglo de estatura y sueldo[%f][%f]", i,j);
-             scanf("%f",&dat[i][j]);
-        }
-    }
-}
-
-int empleadoMasAlto(float dat[N],int *pos)
-{
-    int i;
-    int mayor=dat[0];
-    for (i=0; i<N ; i++ )
-    { 
-        if(dat[i]>mayor)
-        {
-            mayor=dat[i];
-            *pos=i;
-        }
-    }
-    return *pos;
-}
-
-
-void SueldoMenor(float dat[N])
- {
-    int actual,mover;
     float aux;
-    for(actual=0; actual<N-1; actual ++)
+    int auxi;
+    char auxN[20];
+
+    for(i=0; i<N-1; i ++)
     {  
-      for(mover=actual+1; mover<N ;mover++)
+      for(j=0; j<N-1;j++)
        {
-           if (dat[mover]>dat[actual])
+           if (dat[j][0]<dat[j+1][0])
            {
-               aux=dat[mover];
-               dat[mover]=dat[actual];
-               dat[actual]=aux;
+               aux=dat[j][0];
+               dat[j][0]=dat[j+1][0];
+               dat[j+1][0]=aux;
+
+               aux=dat[j][1];
+               dat[j][1]=dat[j+1][1];
+               dat[j+1][1]=aux;
+
+               auxi=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=auxi;
+
+               strcpy(auxN,nom[j]);
+               strcpy(nom[j],nom[j+1]);
+               strcpy(nom[j+1],auxN);
            }   
        }
     }
-    return;
- }
- void imprimir(int arr[N], char nombres[N],float datos[N][M])
+}
+
+void pedirDatos(int arr[N],char nom[N][20],float dat[N][M])
+{
+    int i;
+    for (i=0; i<N; i++ )
+    {
+        printf("dame la clave[%d]", i);
+        scanf("%d", &arr[i]);
+
+        printf("dame el nombre del empleado[%d]", i);
+        scanf("%s", &nom[i]);
+
+        printf("dame la estatura[%d]", i);
+        scanf("%f", &dat[i][0]);
+
+        printf("dame el sueldo[%d]", i);
+        scanf("%f", &dat[i][1]);
+
+
+    }
+
+}
+
+ void imprimir(int arr[N], char nombres[N][20],float datos[N][M])
  {
-     scanf("[%d][%c][%f][%f]",arr,nombres,datos);
+    int i;
+    printf("clave    nombre    estatura   sueldo\n");
+    for (i=0; i<N ; i++ )
+    {
+        printf("%d\t",arr[i]);
+        printf("%s\t",nombres[i]);
+        printf("%f\t",datos[i][0]);
+        printf("%f\n",datos[i][1]);
+    }
  }
+
+
+int empleadoMasAlto(float dat[N][M])
+{
+    int i;
+    float mayor=dat[0][0];
+    int posicion=0;
+    for (i=0; i<N ; i++ )
+    { 
+        if(dat[i][0]>mayor)
+        {
+            mayor=dat[i][0];
+            posicion=i;
+        }
+    }
+    return posicion;
+}
